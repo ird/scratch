beeb8d9de4fc818825cd05117be6d7e5e2678f93
ird <31051194+ird@users.noreply.github.com>
2017-09-16 11:13:35 +0100

    OP2

--- puzzles/operm/src/uk/org/ird/OrderedPermutations2.java
+++ puzzles/operm/src/uk/org/ird/OrderedPermutations2.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.stream.Collectors;
 
 public class OrderedPermutations2 {
-    public LinkedList<Integer> list;
+    private LinkedList<Integer> list;
     public OrderedPermutations2(final LinkedList<Integer> list) {
         this.list = list.stream().sorted().collect(Collectors.toCollection(LinkedList::new));
     }
