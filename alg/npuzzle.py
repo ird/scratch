53422372f4403139e8b4a1e5c04776cdb51c8c32
ird <31051194+ird@users.noreply.github.com>
2017-10-09 22:31:13 +0100

    added movement methods

--- alg/npuzzle.py
+++ alg/npuzzle.py
@@ -1,5 +1,13 @@
 """ npuzzle.py
 N-Puzzle solver """
+from enum import Enum
+
+
+class Direction(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
 
 
 def display(grid, size):
@@ -17,8 +25,44 @@ def display(grid, size):
     return res
 
 
+def move(grid, size, dir):
+    return {
+        Direction.UP: move_up(grid, size),
+        Direction.DOWN: move_down(grid, size),
+        Direction.LEFT: move_left(grid, size),
+        Direction.RIGHT: move_right(grid, size)
+    }[dir]
+
+
+def swap(grid, i, j):
+    grid[i], grid[j] = grid[j], grid[i]
+
+
+def move_up(grid, size):
+    p = grid.index(0)
+    if p < size:
+        pass
+        # TODO: raise OOBException
+    else:
+        swap(grid, p, p - size)
+
+
+def move_down(grid, size):
+    pass
+
+
+def move_left(grid, size):
+    pass
+
+
+def move_right(grid, size):
+    pass
+
+
 def main():
     grid = [2, 1, 7, 4, 3, 6, 5, 0, 8]
+    print(display(grid, 3))
+    move_up(grid, 3)
     print(display(grid, 3))
     pass
 
